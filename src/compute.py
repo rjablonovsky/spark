4107af9e7f941020baf49041a4661da091f1c18d
rjablonovsky <20730145+rjablonovsky@users.noreply.github.com>
2021-04-29 18:23:23 -0600

    modify datatypes

--- src/compute.py
+++ src/compute.py
@@ -1,5 +1,5 @@
 # Read data and do calculations
-# example of run: pyspak < compute.py > log/compute.log 2>&1
+# example of run: pyspark < src/compute.py > log/compute.log 2>&1
 #
 
 from pyspark.sql import SparkSession
@@ -26,25 +26,30 @@ record_holder_wl_by_country_deadlift='analytics/record_holder_wl_by_country_in_d
 df = spark.read.parquet("curated/openpowerlifting")
 
 # show data to browse
-df.select("Name","Sex","Event","Equipment","Age","AgeClass","Division","BodyweightKg","WeightClassKg", \
-          "Squat1Kg","Squat2Kg","Squat3Kg","Squat4Kg","Best3SquatKg","Best3DeadliftKg", \
-		  "Place","Country","MeetCountry","MeetName") \
-		.filter("Best3SquatKg IS NOT NULL").show(20);
+df.select("wlName","Sex","Event","Equipment","Age","AgeClass","Division","BodyweightKg","WeightClassKg", \
+          "wlSquat1Kg","wlSquat2Kg","wlSquat3Kg","Squat4Kg","wlBest3SquatKg","Best3DeadliftKg", \
+		  "wlPlace","wlCountry","Country","MeetCountry","MeetName") \
+		.filter("wlBest3SquatKg IS NOT NULL").show(20);
 
 ### Compute the percentage with which weighlifters achieve their best squad result in their first, second and third attempt each.
 # Approach 1 - with data quality checking and logic inside calculation:
-countBestSquat1Kg = df.filter("Squat1Kg IS NOT NULL AND Squat1Kg > 0 AND Squat1Kg >= coalesce(Squat2Kg,0) AND Squat1Kg >= coalesce(Squat3Kg,0)").count();
-countBestSquat2Kg = df.filter("Squat2Kg IS NOT NULL AND Squat2Kg > 0 AND Squat2Kg > coalesce(Squat1Kg,0) AND Squat2Kg >= coalesce(Squat3Kg,0)").count();
-countBestSquat3Kg = df.filter("Squat3Kg IS NOT NULL AND Squat3Kg > 0 AND Squat3Kg > coalesce(Squat1Kg,0) AND Squat3Kg > coalesce(Squat2Kg,0)").count();
+countBestSquat1Kg = df.filter("wlSquat1Kg IS NOT NULL AND wlSquat1Kg > 0 AND wlSquat1Kg >= coalesce(wlSquat2Kg,0) AND wlSquat1Kg >= coalesce(wlSquat3Kg,0)").count();
+countBestSquat2Kg = df.filter("wlSquat2Kg IS NOT NULL AND wlSquat2Kg > 0 AND wlSquat2Kg > coalesce(wlSquat1Kg,0) AND wlSquat2Kg >= coalesce(wlSquat3Kg,0)").count();
+countBestSquat3Kg = df.filter("wlSquat3Kg IS NOT NULL AND wlSquat3Kg > 0 AND wlSquat3Kg > coalesce(wlSquat1Kg,0) AND wlSquat3Kg > coalesce(wlSquat2Kg,0)").count();
 countBest3SquatKg = countBestSquat1Kg + countBestSquat2Kg + countBestSquat3Kg
 percBestSquat1Kg = round(countBestSquat1Kg * 100.0 / countBest3SquatKg, 2)
 percBestSquat2Kg = round(countBestSquat2Kg * 100.0 / countBest3SquatKg, 2)
 percBestSquat3Kg = round(countBestSquat3Kg * 100.0 / countBest3SquatKg, 2)
+#print(countBest3SquatKg, 100, countBestSquat1Kg, percBestSquat1Kg, countBestSquat2Kg, percBestSquat2Kg, countBestSquat3Kg, percBestSquat3Kg)
 ''' # Approach 2 is more susceptible to data quality issues or require more thorough data cleaning
-countBest3SquatKg = df.filter("Best3SquatKg IS NOT NULL").count();
-percBestSquat1Kg = round(df.filter("Best3SquatKg IS NOT NULL AND Squat1Kg = Best3SquatKg").count()*100/countBest3SquatKg,2)
-percBestSquat2Kg = round(df.filter("Best3SquatKg IS NOT NULL AND Squat2Kg = Best3SquatKg").count()*100/countBest3SquatKg,2)
-percBestSquat3Kg = round(df.filter("Best3SquatKg IS NOT NULL AND Squat3Kg = Best3SquatKg").count()*100/countBest3SquatKg,2)
+countBest3SquatKg = df.filter("(wlBest3SquatKg IS NOT NULL AND wlBest3SquatKg > 0 AND (coalesce(wlSquat1Kg,0) = wlBest3SquatKg OR coalesce(wlSquat2Kg,0) = wlBest3SquatKg OR coalesce(wlSquat3Kg,0) = wlBest3SquatKg))").count()
+countBestSquat1Kg = df.filter("wlBest3SquatKg IS NOT NULL AND wlBest3SquatKg > 0 AND (coalesce(wlSquat1Kg,0) = wlBest3SquatKg)").count()
+countBestSquat2Kg = df.filter("wlBest3SquatKg IS NOT NULL AND wlBest3SquatKg > 0 AND (coalesce(wlSquat2Kg,0) = wlBest3SquatKg)").count()
+countBestSquat3Kg = df.filter("wlBest3SquatKg IS NOT NULL AND wlBest3SquatKg > 0 AND (coalesce(wlSquat3Kg,0) = wlBest3SquatKg)").count()
+percBestSquat1Kg = round(countBestSquat1Kg * 100.0 / countBest3SquatKg, 2)
+percBestSquat2Kg = round(countBestSquat2Kg * 100.0 / countBest3SquatKg, 2)
+percBestSquat3Kg = round(countBestSquat3Kg * 100.0 / countBest3SquatKg, 2)
+print(countBest3SquatKg, 100, countBestSquat1Kg, percBestSquat1Kg, countBestSquat2Kg, percBestSquat2Kg, countBestSquat3Kg, percBestSquat3Kg)
 '''
 # Define schema, import data to dataframe and export data as csv
 percBestSquatSchema = StructType([
@@ -63,19 +68,24 @@ for csvfile in glob.glob(wl_percsquad_in_123_attemp+"/*.csv"):
 shutil.rmtree(wl_percsquad_in_123_attemp)	
 	
 ### Compute the same as above, but only considering participants placing first in their competition.
-# Approach 1 - with data quality checking and logic inside calculation. Assumption is the column Place has correct value for winner of competition.
-countFirstBestSquat1Kg = df.filter("Place = 1 AND Squat1Kg IS NOT NULL AND Squat1Kg > 0 AND Squat1Kg >= coalesce(Squat2Kg,0) AND Squat1Kg >= coalesce(Squat3Kg,0)").count();
-countFirstBestSquat2Kg = df.filter("Place = 1 AND Squat2Kg IS NOT NULL AND Squat2Kg > 0 AND Squat2Kg > coalesce(Squat1Kg,0) AND Squat2Kg >= coalesce(Squat3Kg,0)").count();
-countFirstBestSquat3Kg = df.filter("Place = 1 AND Squat3Kg IS NOT NULL AND Squat3Kg > 0 AND Squat3Kg > coalesce(Squat1Kg,0) AND Squat3Kg > coalesce(Squat2Kg,0)").count();
+# Approach 1 - with data quality checking and logic inside calculation. Assumption is the column wlPlace has correct value for winner of competition.
+countFirstBestSquat1Kg = df.filter("wlPlace = 1 AND wlSquat1Kg IS NOT NULL AND wlSquat1Kg > 0 AND wlSquat1Kg >= coalesce(wlSquat2Kg,0) AND wlSquat1Kg >= coalesce(wlSquat3Kg,0)").count();
+countFirstBestSquat2Kg = df.filter("wlPlace = 1 AND wlSquat2Kg IS NOT NULL AND wlSquat2Kg > 0 AND wlSquat2Kg > coalesce(wlSquat1Kg,0) AND wlSquat2Kg >= coalesce(wlSquat3Kg,0)").count();
+countFirstBestSquat3Kg = df.filter("wlPlace = 1 AND wlSquat3Kg IS NOT NULL AND wlSquat3Kg > 0 AND wlSquat3Kg > coalesce(wlSquat1Kg,0) AND wlSquat3Kg > coalesce(wlSquat2Kg,0)").count();
 countFirstBest3SquatKg = countFirstBestSquat1Kg + countFirstBestSquat2Kg + countFirstBestSquat3Kg
 percFirstBestSquat1Kg = round(countFirstBestSquat1Kg * 100 / countFirstBest3SquatKg, 2)
 percFirstBestSquat2Kg = round(countFirstBestSquat2Kg * 100 / countFirstBest3SquatKg, 2)
 percFirstBestSquat3Kg = round(countFirstBestSquat3Kg * 100 / countFirstBest3SquatKg, 2)
+#print(countFirstBest3SquatKg, 100, countFirstBestSquat1Kg, percFirstBestSquat1Kg, countFirstBestSquat2Kg, percFirstBestSquat2Kg, countFirstBestSquat3Kg, percFirstBestSquat3Kg)
 ''' # Approach 2 is more susceptible to data quality issues or require more thorough data cleaning
-countFirstBest3SquatKg = df.filter("Best3SquatKg IS NOT NULL AND Place = 1").count();
-percFirstBestSquat1Kg = round(df.filter("Best3SquatKg IS NOT NULL AND Place = 1 AND Squat1Kg = Best3SquatKg").count()*100/countFirstBest3SquatKg,2)
-percFirstBestSquat2Kg = round(df.filter("Best3SquatKg IS NOT NULL AND Place = 1 AND Squat2Kg = Best3SquatKg").count()*100/countFirstBest3SquatKg,2)
-percFirstBestSquat3Kg = round(df.filter("Best3SquatKg IS NOT NULL AND Place = 1 AND Squat3Kg = Best3SquatKg").count()*100/countFirstBest3SquatKg,2)
+countFirstBest3SquatKg = df.filter("(wlPlace = 1 AND wlBest3SquatKg IS NOT NULL AND wlBest3SquatKg > 0 AND (coalesce(wlSquat1Kg,0) = wlBest3SquatKg OR coalesce(wlSquat2Kg,0) = wlBest3SquatKg OR coalesce(wlSquat3Kg,0) = wlBest3SquatKg))").count()
+countFirstBestSquat1Kg = df.filter("wlPlace = 1 AND wlBest3SquatKg IS NOT NULL AND wlBest3SquatKg > 0 AND (coalesce(wlSquat1Kg,0) = wlBest3SquatKg)").count()
+countFirstBestSquat2Kg = df.filter("wlPlace = 1 AND wlBest3SquatKg IS NOT NULL AND wlBest3SquatKg > 0 AND (coalesce(wlSquat2Kg,0) = wlBest3SquatKg)").count()
+countFirstBestSquat3Kg = df.filter("wlPlace = 1 AND wlBest3SquatKg IS NOT NULL AND wlBest3SquatKg > 0 AND (coalesce(wlSquat3Kg,0) = wlBest3SquatKg)").count()
+percFirstBestSquat1Kg = round(countFirstBestSquat1Kg * 100.0 / countFirstBest3SquatKg, 2)
+percFirstBestSquat2Kg = round(countFirstBestSquat2Kg * 100.0 / countFirstBest3SquatKg, 2)
+percFirstBestSquat3Kg = round(countFirstBestSquat3Kg * 100.0 / countFirstBest3SquatKg, 2)
+print(countFirstBest3SquatKg, 100, countFirstBestSquat1Kg, percFirstBestSquat1Kg, countFirstBestSquat2Kg, percFirstBestSquat2Kg, countFirstBestSquat3Kg, percFirstBestSquat3Kg)
 '''
 # Define schema, import data to dataframe and export data as csv
 percFirstBestSquatSchema = StructType([
@@ -96,15 +106,13 @@ shutil.rmtree(first_wl_percsquad_in_123_attemp)
 ### For each country, determine the weighlifter who holds the record in deadlifting over the whole dataset.
 # add not null country column to dataset and if Country is null replace it with MeetCountry. 
 #   Assumption 1 - the non-international competition did not provide information about Country
-#   Assumption 2 - the Best3DeadliftKg is reliable enough for identifying recordholder 
-df = df.withColumn('wlCountry', F.coalesce(df['Country'], df['MeetCountry']).cast(StringType()))
-df = df.withColumn('wlBest3DeadliftKg', F.coalesce(df['Best3DeadliftKg'], F.lit('0')).cast(FloatType()))
+#   Assumption 2 - the wlBest3DeadliftKg is reliable enough for identifying recordholder 
 wlRecordInDeadliftKgDF = df.groupBy("wlCountry").agg(F.max("wlBest3DeadliftKg").alias("wlBest3DeadliftKg"))
 wlNameRecordInDeadliftKgDF = wlRecordInDeadliftKgDF.alias("wldf") \
 	.join(df.alias("df"), (F.col("wldf.wlCountry") == F.col("df.wlCountry")) \
 		& (F.col("wldf.wlBest3DeadliftKg") == F.col("df.wlBest3DeadliftKg")),"inner") \
-	.select(F.col("wldf.wlCountry"), F.col("wldf.wlBest3DeadliftKg"), F.col("df.Name")) \
-	.distinct().sort("wlCountry","Name")
+	.select(F.col("wldf.wlCountry"), F.col("wldf.wlBest3DeadliftKg"), F.col("df.wlName")) \
+	.distinct().sort("wlCountry","wlName")
 # save as csv
 wlNameRecordInDeadliftKgDF.coalesce(1).write.format("com.databricks.spark.csv").mode('overwrite') \
 	.option("header", "true").csv(record_holder_wl_by_country_deadlift);
